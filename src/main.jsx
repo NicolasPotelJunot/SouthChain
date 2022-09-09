8b45cc2be55fb267a61c7ac1e34ca388bfc87a9d
NicolasPotelJunot <nicolaspoteljunot@gmail.com>
2022-09-09 00:29:33 -0300

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { BrowserRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import {AppRouters}  from "./routes/AppRouters";
 import { store } from './store/store'
 import { Provider } from 'react-redux'
@@ -9,9 +9,9 @@ import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <AppRouters />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
