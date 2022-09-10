380061f42dda2cd66e0f8c0a17c987d5bca8d517
NicolasPotelJunot <nicolaspoteljunot@gmail.com>
2022-09-09 22:08:01 -0300

    formulario update 2

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,7 @@ export const Form = () => {
   return (
     <form 
     className="grid gap-10 w-full" 
-    action="https://formsubmit.co/286e37e6f3aa29152e1e3598b52717d9" 
+    action="https://formsubmit.co/nicolaspoteljunot@gmail.com" 
     method="POST"
 
     >
@@ -20,7 +20,7 @@ export const Form = () => {
         <div className="w-3/4 xl:w-1/4 m-auto">
             <Input name="tel" label="TELEFONO" required type="tel"/>
         </div>
-        <input type="hidden" name="_next" value="http://127.0.0.1:5173/#/contact" />
+        <input type="hidden" name="_next" value="https://nicolaspoteljunot.github.io/SouthChain/?#/contact" />
         <input type="hidden" name="_captcha" value="false" />
         <Button type="submit" className="bg-gradient w-20 flex py-2 justify-center items-center m-auto text-base">ENVIAR</Button>
     </form>
